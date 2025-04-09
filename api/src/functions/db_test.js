1a8d2e92b5b9769fc4d3dfab30ed9316c6cfc7f7
Demian Holmberg <dholmberg@gmail.com>
2025-04-09 16:54:23 -0400

    refactor: remove unused database connection utilities and query files

--- api/src/functions/db_test.js
+++
@@ -1,9 +0,0 @@
-const { app } = require('@azure/functions');
-
-const { apiRun } = require('../connectionUtils.js');
-const { getDriverOfDay } = require('../queryObjects/driverQueries.js');
-
-app.http(
-    'db_test',
-    apiRun(getDriverOfDay)
-);
